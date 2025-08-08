c5a506ddb6c9f8f84a4b5d6a97500a0359bafc1c
Shreeya M <shreeya.pmanwatkar@gmail.com>
2025-08-08 15:37:26 +0530

    Add files via upload

---
+++ Task13.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+import java.util.Vector;
+
+public class Task13 {
+	public static void main(String[] args) {
+		Vector<String> Mercedes = new Vector<String>();
+		Mercedes.add("S-Class");
+		Mercedes.add("E-Class");
+		Mercedes.add("G-Wagon");
+		
+		System.out.println(Mercedes);
+	}
+}
